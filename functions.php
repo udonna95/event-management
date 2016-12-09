2e3b5f117a23d2af999d549b79dadd80e5b9a56b
udonna95 <udonnaobi@eduroam-int-dhcp-97-3-186.ucl.ac.uk>
2016-12-09 16:46:49 +0000

    initial commit

---
+++ functions.php
@@ -0,0 +1,58 @@
+<?php // Example 26-1: functions.php
+$user = 'root';
+$password = 'udonna95';
+$db = 'Sample2';
+$host = '127.0.0.1';
+
+$connection =  new mysqli($host,$user,$password,$db);
+if($connection->connect_error) die($connection->connect_error);
+  $appname = "Robin's Nest"; // ...and preference
+
+
+  function createTable($name, $query)
+  {
+    queryMysql("CREATE TABLE IF NOT EXISTS $name($query)");
+    echo "Table '$name' created or already exists.<br>";
+  }
+
+  function queryMysql($query)
+  {
+    global $connection;
+    $result = $connection->query($query);
+    if (!$result) die($connection->error);
+    return $result;
+  }
+
+  function destroySession()
+  {
+    $_SESSION=array();
+
+    if (session_id() != "" || isset($_COOKIE[session_name()]))
+      setcookie(session_name(), '', time()-2592000, '/');
+
+    session_destroy();
+  }
+
+  function sanitizeString($var)
+  {
+    global $connection;
+    $var = strip_tags($var);
+    $var = htmlentities($var);
+    $var = stripslashes($var);
+    return $connection->real_escape_string($var);
+  }
+
+  function showProfile($user)
+  {
+    if (file_exists("$user.jpg"))
+      echo "<img src='$user.jpg' style='float:left;'>";
+
+    $result = queryMysql("SELECT * FROM profiles WHERE user='$user'");
+
+    if ($result->num_rows)
+    {
+      $row = $result->fetch_array(MYSQLI_ASSOC);
+      echo stripslashes($row['text']) . "<br style='clear:left;'><br>";
+    }
+  }
+?>
